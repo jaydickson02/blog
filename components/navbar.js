92d61e78b1b5f323d86db0aee8ae9e4f7a7cb586
Jay Dickson <jaydickson02@gmail.com>
2022-12-06 14:36:59 +1100

    Fix navigation behaviour

--- components/navbar.js
+++ components/navbar.js
@@ -9,104 +9,104 @@ import Link from 'next/link'
 
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+    return classes.filter(Boolean).join(' ')
 }
 
 export default function Example(props) {
 
     const navigation = [
+        { name: 'All', href: '/', current: false },
         { name: 'Dev', href: '/category/dev', current: false },
-        { name: 'One Day', href: '/category/someday', current: false },
         { name: 'Engineering', href: '/category/engineering', current: false },
         { name: 'Mathematics', href: '/category/math', current: false },
         { name: 'Message Board', href: '/messageboard', current: false },
-        
-      ]
 
-      for(let i = 0; i < navigation.length; i++){
-          if(navigation[i].name == props.active){
-              navigation[i].current = true;
-          }
-      }
+    ]
 
-  return (
-    <Disclosure as="nav" className="bg-white-800">
-      {({ open }) => (
-        <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex-shrink-0 flex items-center">
-                <Link href="/">
-                    <a className="hover:bg-gray-700 hover:text-white hover:rounded-md text-md px-3 py-1.5">
-                        <FontAwesomeIcon icon={faRocket} />
-                        <span className='px-2'>Jay Dickson</span>
-                    </a>
-                </Link>
-                </div>
+    for (let i = 0; i < navigation.length; i++) {
+        if (navigation[i].name == props.active) {
+            navigation[i].current = true;
+        }
+    }
 
-                <div className="hidden sm:block sm:ml-6">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                    <Link key={item.name} href={item.href}>
-                      <a
-                        className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-black-300 hover:bg-gray-700 hover:text-white',
-                          'px-3 py-2 rounded-md text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    </Link>
-                    ))}
-                  </div>
-                </div>
-              </div>
-              
-            </div>
-          </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="px-8 pt-2 pb-3 space-y-2">
-              {navigation.map((item) => (
-                    
-                    <Disclosure.Button
-                    key={item.name}
-                    as="button"
-                    className={classNames(
-                        item.current ? 'bg-gray-900 text-white' : 'text-black-300 hover:bg-gray-700 hover:text-white',
-                        'block px-3 py-2 rounded-md text-base font-medium'
-                    )}
-                    aria-current={item.current ? 'page' : undefined}>
-                        <Link href={item.href}>
-                            <div className="relative flex">
-                                <span className="w-96 text-left">{item.name}</span>
+    return (
+        <Disclosure as="nav" className="bg-white-800">
+            {({ open }) => (
+                <>
+                    <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                        <div className="relative flex items-center justify-between h-16">
+                            <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+                                {/* Mobile menu button*/}
+                                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="sr-only">Open main menu</span>
+                                    {open ? (
+                                        <XIcon className="block h-6 w-6" aria-hidden="true" />
+                                    ) : (
+                                        <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                                    )}
+                                </Disclosure.Button>
                             </div>
-                        </Link>
-                    
-                    </Disclosure.Button>
-                    
-                
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
-  )
+
+                            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                                <div className="flex-shrink-0 flex items-center">
+                                    <Link href="https://www.jaydickson.net/">
+                                        <a className="hover:bg-gray-700 hover:text-white hover:rounded-md text-md px-3 py-1.5">
+                                            <FontAwesomeIcon icon={faRocket} />
+                                            <span className='px-2'>Jay Dickson</span>
+                                        </a>
+                                    </Link>
+                                </div>
+
+                                <div className="hidden sm:block sm:ml-6">
+                                    <div className="flex space-x-4">
+                                        {navigation.map((item) => (
+                                            <Link key={item.name} href={item.href}>
+                                                <a
+                                                    className={classNames(
+                                                        item.current ? 'bg-gray-900 text-white' : 'text-black-300 hover:bg-gray-700 hover:text-white',
+                                                        'px-3 py-2 rounded-md text-sm font-medium'
+                                                    )}
+                                                    aria-current={item.current ? 'page' : undefined}
+                                                >
+                                                    {item.name}
+                                                </a>
+                                            </Link>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <Disclosure.Panel className="sm:hidden">
+                        <div className="px-8 pt-2 pb-3 space-y-2">
+                            {navigation.map((item) => (
+
+                                <Disclosure.Button
+                                    key={item.name}
+                                    as="button"
+                                    className={classNames(
+                                        item.current ? 'bg-gray-900 text-white' : 'text-black-300 hover:bg-gray-700 hover:text-white',
+                                        'block px-3 py-2 rounded-md text-base font-medium'
+                                    )}
+                                    aria-current={item.current ? 'page' : undefined}>
+                                    <Link href={item.href}>
+                                        <div className="relative flex">
+                                            <span className="w-96 text-left">{item.name}</span>
+                                        </div>
+                                    </Link>
+
+                                </Disclosure.Button>
+
+
+                            ))}
+                        </div>
+                    </Disclosure.Panel>
+                </>
+            )}
+        </Disclosure>
+    )
 }
 
 
--- pages/index.js
+++ pages/index.js
@@ -13,15 +13,15 @@ class Index extends React.Component {
         super(props)
         this.myRef = React.createRef()
         this.state = {
-          posts: props.posts,
-          search: ''
+            posts: props.posts,
+            search: ''
         };
 
         this.handleChange = this.handleChange.bind(this);
-      }
-    
-    handleChange(event){
-        this.setState({search: event.target.value});
+    }
+
+    handleChange(event) {
+        this.setState({ search: event.target.value });
     }
 
     render() {
@@ -30,21 +30,19 @@ class Index extends React.Component {
             <Layout active="/">
 
                 <div className="flex justify-center md:justify-start ml-2">
-                <div className="mb-3 xl:w-96">
-                    <div className="input-group relative flex items-stretch w-full mb-4 rounded">
-                    
-                    <input type="search" className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search..." aria-label="Search" aria-describedby="button-addon2" value={this.state.search} onChange={this.handleChange}></input>
+                    <div className="mb-3 xl:w-96">
+                        <div className="input-group relative flex items-stretch w-full mb-4 rounded">
 
-                    <span className="input-group-text flex items-center px-3 py-1.5 text-base font-normal text-gray-700 text-center whitespace-nowrap rounded" id="basic-addon2">
-                    <FontAwesomeIcon icon={faMagnifyingGlass} />    
-                    </span>
+                            <input type="search" className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search..." aria-label="Search" aria-describedby="button-addon2" value={this.state.search} onChange={this.handleChange}></input>
+
+
+                        </div>
                     </div>
                 </div>
-                </div>
-                <Posts search={this.state.search} posts={this.state.posts}/>
+                <Posts search={this.state.search} posts={this.state.posts} />
             </Layout>
         );
-  }
+    }
 }
 
 
@@ -54,8 +52,8 @@ export async function getStaticProps() {
 
     let files = [];
 
-    for(let i = 0; i < allFiles.length; i++){
-        if(allFiles[i].search(".md") != '-1'){
+    for (let i = 0; i < allFiles.length; i++) {
+        if (allFiles[i].search(".md") != '-1') {
             files.push(allFiles[i])
         }
     }
@@ -64,18 +62,18 @@ export async function getStaticProps() {
         const slug = fileName.replace('.md', '');
         const readFile = fs.readFileSync(`posts/${fileName}`, 'utf-8');
         const { data: frontmatter } = matter(readFile);
-        
+
         return {
-          slug,
-          frontmatter,
+            slug,
+            frontmatter,
         };
     });
 
     return {
         props: {
-          posts,
+            posts,
         },
     };
-  }
+}
 
-  export default Index
+export default Index
