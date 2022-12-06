404d4dbaa58f05eef05f30681afa0c19b1bd9d11
Jay Dickson <jaydickson02@gmail.com>
2022-12-06 23:57:07 +1100

    Tidied Code

--- components/banner.js
+++ components/banner.js
@@ -2,21 +2,21 @@
 import { SpeakerphoneIcon, XIcon } from '@heroicons/react/outline'
 
 export default function banner(props) {
-  return (
-    <div className="bg-indigo-600">
-      <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between flex-wrap">
-          <div className="w-0 flex-1 flex items-center">
-            <span className="flex p-2 rounded-lg bg-indigo-800">
-              <SpeakerphoneIcon className="h-6 w-6 text-white" aria-hidden="true" />
-            </span>
-            <p className="ml-3 font-medium text-white truncate">
-              <span className="md:hidden">{props.content}</span>
-              <span className="hidden md:inline">{props.content}</span>
-            </p>
-          </div>
+    return (
+        <div className="bg-indigo-600">
+            <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+                <div className="flex items-center justify-between flex-wrap">
+                    <div className="w-0 flex-1 flex items-center">
+                        <span className="flex p-2 rounded-lg bg-indigo-800">
+                            <SpeakerphoneIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                        </span>
+                        <p className="ml-3 font-medium text-white truncate">
+                            <span className="md:hidden">{props.content}</span>
+                            <span className="hidden md:inline">{props.content}</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
